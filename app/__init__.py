33d3080c52ba5819c955d27aa35ea514c5f310af
Prajjwol <prajj.mondal@gmail.com>
2020-04-10 18:37:17 -0400

    broke up application into blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,23 @@ from config import Config
 from flask_bcrypt  import Bcrypt
 from flask_login import LoginManager
 from app.db import DB
+from app.errors import error_blueprint
+
 
 application = Flask(__name__)
 application.config.from_object(Config)
+application.register_blueprint(error_blueprint)
 bcrypt = Bcrypt(application)
 login = LoginManager(application)
-login.login_view = 'login'
+login.login_view = 'auth_blueprint.login'
 # The 'login' value above is the function (or endpoint) name for the login view. 
 # In other words, the name you would use in a url_for() call to get the URL.
 db = DB(application)
 
-from app import routes, models, errors
+from app.auth import auth_blueprint
+application.register_blueprint(auth_blueprint, url_prefix="/auth")
+
+from app.main import main_blueprint
+application.register_blueprint(main_blueprint)
+
+from app import models
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,29 +1,26 @@
-from app import application, bcrypt, db
+from app import bcrypt, db
+from app.auth import auth_blueprint
+from app.main import main_blueprint
 from app.models import User
-from app.forms import LoginForm, SignupForm
+from app.auth.forms import LoginForm, SignupForm
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
-@application.route('/')
-@application.route('/index')
-def index():
-     return render_template('index.html', title="Home Page")
-
-@application.route('/login', methods=['GET', 'POST'])
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main_blueprint.index'))
     form = LoginForm()
     if form.validate_on_submit():
         db_user_object = db.get_user_by_username(form.username.data)
         if db_user_object is None:
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth_blueprint.login'))
         user = User(db_user_object['username'], db_user_object['password'], db_user_object['_id'], db_user_object['email'])
         if not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth_blueprint.login'))
         login_user(user, remember=form.remember_me.data)
 
         # An attacker could insert a URL to a malicious site in the next argument, so the application only redirects
@@ -32,16 +29,16 @@ def login():
         # and then check if the netloc component is set or not.
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main_blueprint.index')
         return redirect(next_page)
-    return render_template('login.html', title="Log in", form=form)
+    return render_template('auth/login.html', title="Log in", form=form)
 
-@application.route('/logout')
+@auth_blueprint.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main_blueprint.index'))
 
-@application.route('/signup', methods=['GET', 'POST'])
+@auth_blueprint.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
@@ -49,11 +46,4 @@ def signup():
         print(f"encrypted_pwd: {encrypted_pwd}")
         db.add_user({'username': form.username.data, 'password': str(encrypted_pwd), 'email': form.email.data})
         return redirect(url_for('index'))
-    return render_template('signup.html', title="Sign in", form=form)
-
-@application.route('/user/<username>')
-@login_required
-def get_user_profile(username):
-    db_user_object = db.get_user_by_username(username)
-    user = User(db_user_object['username'], db_user_object['password'], db_user_object['_id'], db_user_object['email'])
-    return render_template('user_profile.html', user=user)
+    return render_template('auth/signup.html', title="Sign in", form=form)
